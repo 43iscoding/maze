6913c448de6e12b19311171b4889fa96b0009384
Shiju Kodo <43iscoding@gmail.com>
2014-06-14 00:56:20 +0300

    Cell may now contain multiple objects

--- js/maze.js
+++ js/maze.js
@@ -6,6 +6,49 @@
 
     var DEBUG = false;
 
+    function Cell(index, type) {
+        this.index = index;
+        this.pos = getXY(index);
+        this.objects = [ type ];
+        if (type == CELL.PLAYER) {
+            this.objects.push(CELL.EMPTY);
+        }
+    }
+
+    Cell.prototype = {
+        addObject : function(object) {
+            this.objects.push(object);
+        },
+        removeObject : function(object) {
+            var index = this.objects.indexOf(object);
+            if (index != -1) {
+                this.objects.splice(index, 1);
+            }
+        },
+        contains: function(toFind) {
+            var found = false;
+            this.objects.forEach(function(object) {
+                if (object == toFind) found = true;
+            });
+            return found;
+        },
+        getX : function() { return this.pos.x; },
+        getY : function() { return this.pos.y; },
+        getValue : function() {
+            var value = { cell: CELL.UNKNOWN, priority : getPriority(CELL.UNKNOWN)};
+            this.objects.forEach(function(object) {
+                if (value.priority < getPriority(object)) {
+                    value = { cell : object, priority : getPriority(object)};
+                }
+            });
+            return value.cell;
+        },
+        toString: function() {
+            return this.getValue();
+        },
+        constructor : Cell
+    };
+
     /**
      * Maze map.
      *
@@ -17,9 +60,11 @@
      * H - Hospital
      * P - Player
      * @ - Out of bounds
+     * ~ - Bullet
      */
 
     window.CELL = {
+        UNKNOWN : '?',
         EMPTY : '.',
         WALL : '^',
         OUTER_WALL : '*',
@@ -27,16 +72,42 @@
         ARSENAL : 'A',
         HOSPITAL : 'H',
         PLAYER : 'P',
-        OUT_OF_BOUNDS : '@'
+        OUT_OF_BOUNDS : '@',
+        BULLET: '~'
     };
 
-    var map =
+    function getPriority(object) {
+        switch (object) {
+            case CELL.OUT_OF_BOUNDS: return 6;
+            case CELL.PLAYER: return 5;
+            case CELL.ARSENAL:
+            case CELL.HOSPITAL:
+            case CELL.EXIT: return 4;
+            case CELL.OUTER_WALL: return 3;
+            case CELL.WALL: return 2;
+            case CELL.BULLET: return 1;
+            case CELL.EMPTY: return 0;
+            case CELL.UNKNOWN: return -1;
+        }
+    }
+
+    function createLevel(level) {
+        var newMap = [];
+        for (var i = 0; i < level.length; i++) {
+            newMap.push(new Cell(i, level[i]));
+        }
+        return newMap;
+    }
+
+    var level1 =
         '******' +
         '*....*' +
         '*.P..E' +
         '*...H*' +
         '*..A.*' +
-        '******';
+        '******' ;
+
+    var map = createLevel(level1);
 
     window.init = function() {
         console.log('Game loaded!');
@@ -100,10 +171,18 @@
         var targetCell = cellAt(target);
         if (targetCell == CELL.WALL || targetCell == CELL.OUTER_WALL) return RESULT.WALL;
 
-        updateMap(getPlayer(), CELL.EMPTY);
-        updateMap(target, CELL.PLAYER);
+        remove(getPlayer(), CELL.PLAYER);
+        add(target, CELL.PLAYER);
 
         return RESULT.OK;
+    }
+
+    function remove(index, value) {
+        map[index].removeObject(value);
+    }
+
+    function add(index, value) {
+        map[index].addObject(value);
     }
 
     function updateMap(index, value) {
@@ -138,7 +217,7 @@
 
     function getPlayer() {
         for (var i = 0; i < map.length; i++) {
-            if (map[i] == CELL.PLAYER) return i;
+            if (map[i].contains(CELL.PLAYER)) return i;
         }
         console.log("COULD NOT FIND PLAYER!");
         return -1;
@@ -149,7 +228,7 @@
         mapContainer.style.width = "15px";
         mapContainer.innerHTML = "";
         for (var i = 0; i < map.length; i++) {
-            mapContainer.innerHTML += map[i];
+            mapContainer.innerHTML += map[i].getValue();
             if ((i + 1) % side == 0) {
                 mapContainer.innerHTML += '\n';
             }

60fc1b1557ee54a274d85720e63c7422a89c2c25
Shiju Kodo <43iscoding@gmail.com>
2014-06-24 22:25:24 +0300

    Typewriter added

--- js/console.js
+++ js/console.js
@@ -82,11 +82,32 @@
         }
     }
 
+    var typeWriter;
+
     function print(key) {
         var output = document.getElementById('consoleOutput');
         output.style.color = getColor();
-        output.innerHTML = get(key);
+        if (typeWriter) typeWriter.clear();
+        typeWriter = new Typewriter(output, get(key));
     }
+
+    function Typewriter(element, text) {
+        var delay = [30, 60];
+        element.innerHTML = '';
+        var write = function() {
+            if (text.length == 0) return;
+            element.innerHTML += text[0];
+            text = text.substring(1);
+        };
+
+        this.interval = setInterval(write, delay[0] + Math.random() * (delay[1] - delay[0]));
+    }
+
+    Typewriter.prototype = {
+        clear : function() {
+            clearInterval(this.interval);
+        }
+    };
 
     function get(key) {
         if (dictionary[key] == undefined) {
